1dd7644936baa2ec033c6e2c63d583a21729cf24
azizulhoq953 <azizulhoq2041@gmail.com>
2021-11-20 12:59:10 +0600

    statefull

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -1,20 +1,44 @@
+// ignore_for_file: use_key_in_widget_constructors, avoid_unnecessary_containers
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: 'Just Practice',
-    home: Scaffold(body: MyApp()),
+    home: Scaffold(body: MyApp2()),
   ));
 }
 
+class MyApp2 extends StatefulWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  @override
+  _MyApp2State createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+          child: Text(
+        'Hello I am Statefull Widget',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      )),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [
+      children: const [
         CustomButton('ok'),
         SizedBox(
           height: 10,
@@ -35,19 +59,20 @@ class MyApp extends StatelessWidget {
 
 class CustomButton extends StatelessWidget {
   final String title;
-  CustomButton(this.title);
+  const CustomButton(this.title);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
         onTap: () {
+          // ignore: avoid_print
           print('onTap');
         },
         child: Container(
           height: 40,
           width: 100,
-          decoration:
-              BoxDecoration(shape: BoxShape.rectangle, color: Colors.blue),
+          decoration: const BoxDecoration(
+              shape: BoxShape.rectangle, color: Colors.blue),
           child: Center(child: Text(title)),
         ));
   }
