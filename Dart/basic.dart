05ed8ec5177d56752f3f456fb2512a8766af8a42
azizulhoq953 <azizulhoq2041@gmail.com>
2022-10-01 12:59:12 +0600

    modified

---
+++ Dart/basic.dart
@@ -0,0 +1,18 @@
+class Wordp{
+int iD=120;
+  void funct(){
+  print("SucessFully Print");
+    print(iD);
+}
+}
+
+
+
+void main(){
+  
+  Wordp w= Wordp();
+  
+  //w.iD(12021002);
+ w.funct();
+  
+}
