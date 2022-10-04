e967f2b766f1f40230b338be6a64cd84310869c0
azizulhoq953 <azizulhoq2041@gmail.com>
2022-10-04 12:08:30 +0600

    prt

--- Flutter-project/basic/lib/main.dart
+++ Flutter-project/basic/lib/main.dart
@@ -1,22 +1,44 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main()=> runApp(MyApp());
+
+// class MyApp extends StatelessWidget{
+//   @override 
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+// home:  SafeArea(
+//   child:   Scaffold( 
+  
+//     backgroundColor: Colors.pink,
+  
+//      body: Center(child: Text("I Love Coding",style: TextStyle(fontSize: 35),)),
+  
+  
+//    ),
+// ),
+//     );
+//   }
+// }
+
+
 
 class MyApp extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-home:  SafeArea(
-  child:   Scaffold( 
-  
-    backgroundColor: Colors.pink,
-  
-     body: Center(child: Text("I Love Coding",style: TextStyle(fontSize: 35),)),
-  
-   ),
-),
+      home:SafeArea(
+        child: Scaffold( 
+          body: Container( 
+            height: 200,
+            width: 300,
+            decoration: BoxDecoration(color:Colors.pink),
+            child:Center(child: Text("I Love Coding I'm New This FrameWork Flutter",style: TextStyle(fontSize: 25, ),  )),
+          ),
+        ),
+      ),
     );
   }
 }
-
